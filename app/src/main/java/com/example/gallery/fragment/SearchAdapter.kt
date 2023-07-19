d05d69bedbc7702b2edab4420d18e1b27e14e90e
zxr <2293226903@qq.com>
2023-07-19 21:01:37 +0800

    这次提交主要制定了项目的整体页面数量和内容。具体来说添加了搜索页面，和搜索结果展示。逻辑等完成主页面优化后再写。还完成了搜索页面和搜索结果展示页面。总体来说是一个相对完整的app。后续的数据读取和本地存储后面再写。

---
+++ app/src/main/java/com/example/gallery/fragment/SearchAdapter.kt
@@ -0,0 +1,95 @@
+package com.example.gallery.fragment
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.example.gallery.R
+import com.example.gallery.modul.Photo
+import io.supercharge.shimmerlayout.ShimmerLayout
+
+
+class SearchAdapter(private val photoList: List<Photo>):
+    RecyclerView.Adapter<SearchAdapter.ViewHold>() {
+
+    inner class ViewHold(view: View):RecyclerView.ViewHolder(view){
+        val photoImage:ImageView=view.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHold {
+        val view= LayoutInflater.from(parent.context)
+            .inflate(R.layout.gallery_cell,parent,false)
+        return ViewHold(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHold, position: Int) {
+        //先获得shimmerLayout的实例，然后对其属性进行设置
+        holder.itemView.findViewById<ShimmerLayout>(R.id.shimmerGellLayout).apply {
+            //设置占位图片的颜色
+            setShimmerColor(0x55FFFFFF)
+            //设置闪动的角度
+            setShimmerAngle(45)
+            //开启闪动
+            startShimmerAnimation()
+        }
+        //拿到此时该图片的数据
+        val photo=photoList[position]
+        //使用Glide放置图片
+        Glide.with(holder.itemView)
+            .load(photo.webformatURL)
+            //设置占位图片
+            .placeholder(R.drawable.ic_grayphoto_24)
+            //设置一个监听器，用于监测是否放置成功
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+                //无论是否放置成功监听器的值必须返回false，return图片就加载不出来了
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false.also {
+                        //放置成功后关闭shimmer
+                        holder.itemView.findViewById<ShimmerLayout>(R.id.shimmerGellLayout)
+                            ?.stopShimmerAnimation()
+                        //注意这里需要加一个对shimmerLayout的判空处理
+                        //因为如果我们在图片加载完成之前从该页面离开，但是这个listener还是会执行
+                        //就会遇见空指针异常，导致程序崩溃
+                    }
+                }
+            })
+            //加载到的控件的
+            .into(holder.itemView.findViewById(R.id.imageView))
+
+        holder.itemView.setOnClickListener {
+            //由于我们是采用Navigation的方式从一个fragment转跳到另外一个fragment当中去
+            //这里数据的传输我们采用的是bundle的方式
+            val bundle= Bundle()
+            bundle.apply {
+                putString("largeImageURL",photo.largeImageURL)
+                //转跳到图片详情的页面
+                holder.itemView.findNavController().navigate(R.id.action_resultFragment_to_photoFragment2,bundle)
+            }
+        }
+    }
+
+    override fun getItemCount()=photoList.size
+}
